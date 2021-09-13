c165774c6035aac0dbbd4d9e8e42d3477c96ac77
VictorDamian <vdamianbarron@gmail.com>
2021-09-13 17:25:00 -0500

    interaccion query login

---
+++ MVCEmployee/MVCEmployee/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEmployee.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Access(string user, string pass)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch(Exception e)
+            {
+                return Content("Error al entrar al sistema" + e.Message);
+            }
+        }
+    }
+}
