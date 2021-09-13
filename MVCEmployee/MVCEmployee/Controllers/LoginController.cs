0cd8c263db85742714f3baec074c1541e0c86c7d
VictorDamian <vdamianbarron@gmail.com>
2021-09-13 17:52:06 -0500

    Conexion a bd con EntityFramework

--- MVCEmployee/MVCEmployee/Controllers/LoginController.cs
+++ MVCEmployee/MVCEmployee/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCEmployee.Models;
 
 namespace MVCEmployee.Controllers
 {
@@ -17,7 +18,24 @@ namespace MVCEmployee.Controllers
         {
             try
             {
-                return Content("1");
+                using (PRACTICAMVCEntities db = new PRACTICAMVCEntities())
+                {
+                    var lst = from i in db.USERS
+                              where i.USERNAME == user && i.PASSWORD == pass && i.IDSTATE == 1
+                              orderby i.USERNAME
+                              select i;
+                    if (lst.Count() > 0)
+                    {
+                        USERS objUser = lst.First();
+                        Session["USERS"] = objUser;
+                        return Content("1");
+                    }
+                    else
+                    {
+                        return Content("Ha ocurrido un error...");
+                    }
+                }
+                
             }
             catch(Exception e)
             {
