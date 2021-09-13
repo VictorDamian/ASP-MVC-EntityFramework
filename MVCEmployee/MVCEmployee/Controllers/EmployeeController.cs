520c18049ebfbfef353217452d87f9e182dbb34d
VictorDamian <vdamianbarron@gmail.com>
2021-09-13 18:49:54 -0500

    agregando metodo para consultar empleados

---
+++ MVCEmployee/MVCEmployee/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using MVCEmployee.Models.EmployeeModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCEmployee.Models;
+
+namespace MVCEmployee.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            List<Employee> employees = null;
+            using (PRACTICAMVCEntities db = new PRACTICAMVCEntities())
+            {
+                employees = (from i in db.EMPLOYEE
+                             orderby i.NAME
+                             select new Employee
+                              {
+                                 name = i.NAME,
+                                 age = i.AGE,
+                                 email = i.MAIL,
+                                 position = i.POSITION
+                              }).ToList();
+            }
+                return View(employees);
+        }
+    }
+}
---
+++ MVCEmployee/MVCEmployee/Models/EmployeeModel/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCEmployee.Models.EmployeeModel
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string email { get; set; }
+        public string position { get; set; }
+    }
+}
