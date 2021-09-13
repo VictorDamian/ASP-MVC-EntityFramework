e085550ccf71e8d691de985f5dbd61be7056438b
VictorDamian <vdamianbarron@gmail.com>
2021-09-13 18:02:18 -0500

    Filtro de seguridad en login

---
+++ MVCEmployee/MVCEmployee/Filters/VerifySession.cs
@@ -0,0 +1,33 @@
+﻿using MVCEmployee.Controllers;
+using MVCEmployee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEmployee.Filters
+{
+    public class VerifySession:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var objUser = (USERS)HttpContext.Current.Session["USERS"];
+            if(objUser == null)
+            {
+                if (filterContext.Controller is LoginController == false)
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Login/Index");
+                }
+            }
+            else
+            {
+                if (filterContext.Controller is LoginController == true)
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Home/Index");
+                }
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
