ffa717526339625263e66dd4f231fd9289c4d3ae
VictorDamian <vdamianbarron@gmail.com>
2021-09-13 22:11:31 -0500

    Funcion cerrar

---
+++ MVCEmployee/MVCEmployee/Controllers/CloseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEmployee.Controllers
+{
+    public class CloseController : Controller
+    {
+        // GET: Close
+        public ActionResult Close()
+        {
+            Session["USERS"] = null;
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
