619a882839d44d8e684b0ece025fee34758cf3a1
VictorDamian <vdamianbarron@gmail.com>
2021-09-13 22:05:25 -0500

    Metodo editar y eliminar

--- MVCEmployee/MVCEmployee/Controllers/EmployeeController.cs
+++ MVCEmployee/MVCEmployee/Controllers/EmployeeController.cs
@@ -20,14 +20,15 @@ namespace MVCEmployee.Controllers
                 employees = (from i in db.EMPLOYEE
                              orderby i.NAME
                              select new Employee
-                              {
+                             {
+                                 id = i.IDEMP,
                                  name = i.NAME,
                                  age = i.AGE,
                                  email = i.MAIL,
                                  position = i.POSITION
-                              }).ToList();
+                             }).ToList();
             }
-                return View(employees);
+            return View(employees);
         }
         [HttpGet]
         public ActionResult Add()
@@ -52,6 +53,52 @@ namespace MVCEmployee.Controllers
                 db.SaveChanges();
             }
             return Redirect(Url.Content("~/Employee/"));
+        }
+        // GET DATA
+        public ActionResult Update(int id)
+        {
+            EmployeeModel model = new EmployeeModel();
+            using (var db = new PRACTICAMVCEntities())
+            {
+                var oEmployee = db.EMPLOYEE.Find(id);
+                model.Id = oEmployee.IDEMP;
+                model.Name = oEmployee.NAME;
+                model.Age = oEmployee.AGE;
+                model.Email = oEmployee.MAIL;
+                model.Position = oEmployee.POSITION;
+            }
+
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Update(EmployeeModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            using (var db = new PRACTICAMVCEntities())
+            {
+                var oEmployee = db.EMPLOYEE.Find(model.Id);
+                oEmployee.NAME = model.Name;
+                oEmployee.AGE = model.Age;
+                oEmployee.MAIL = model.Email;
+                oEmployee.POSITION = model.Position;
+                db.Entry(oEmployee).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            return Redirect(Url.Content("~/Employee/"));
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (var db = new PRACTICAMVCEntities())
+            {
+                var ob = db.EMPLOYEE.Find(id);
+                db.Entry(ob).State = System.Data.Entity.EntityState.Deleted;
+                db.SaveChanges();
+            }
+            return Content("1");
         }
     }
 }
